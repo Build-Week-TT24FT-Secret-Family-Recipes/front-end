ec41e69ee24cffe714d726e683d186526f95198a
Bansa Laura <weidiwu207@gmail.com>
2021-03-03 17:07:18 -0500

    removed some stuff and added some stuff

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function HomePage() {
+export const HomePage = (props) => {
     return (
         <div>
             
         </div>
     )
 }
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
