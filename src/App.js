d128608802f7f33661e722e0645b56fbad1efa9a
Wei Wu <weidiwu207@gmail.com>
2021-03-02 23:09:13 -0500

    Test (#2)
    * added files
    
    * added some links/files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Login from "./components/Login";
+import Home from './components/Home'
 import GetUserInfo from "./components/UserInfo";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
@@ -8,8 +9,17 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<Route exact path="/" component={Login} />
-			<ProtectedRoute exact path="/userinfo" component={GetUserInfo} />
+			<nav>
+			<Link to ='/'>
+				Home
+			</Link>
+			<Link to ='/login'>
+				Login
+			</Link>
+			</nav>
+			<Route exact path="/" component={Home} />
+			<Route exact path='/login' component={Login} />
+			{/* <ProtectedRoute exact path="/userinfo" component={GetUserInfo} /> */}
 		</div>
 	);
 }
