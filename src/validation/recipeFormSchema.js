ba09d59547dc618612e3cad0067899a730853290
cynthicrush <cynthiazcanada@gmail.com>
2021-03-04 16:38:03 -0500

    formSchema changed and EditRecipeForm added

---
+++ src/validation/recipeFormSchema.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup';
+
+const recipeFormSchema = yup.object().shape({
+    title: yup.string()
+        .required('Title is required.'),
+    source: yup.string()
+        .required('Source is required.'),
+    ingredients: yup.string()
+        .required('Ingredients is required.'),
+    instructions: yup.string()
+        .required('Instructions is required.'),
+    category: yup.string()
+        .required('Category is required.'),
+});
+export default recipeFormSchema;
